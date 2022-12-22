17f14d497803d360a3be5b0a120ab537573fa6af
pntech20 <pntech20@gmail.com>
2022-12-22 07:31:16 +0700

    Add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import TagManager from 'react-gtm-module';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  // https://www.learnbestcoding.com/post/9/easiest-way-to-integrate-google-analytics-with-react-js-and-next-js#pageviews
+  useEffect(() => {
+    TagManager.initialize({ gtmId: String(process.env.NEXT_PUBLIC_GTM_ID) });
+  }, []);
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
