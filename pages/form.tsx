f573232f8db033ce2c68a9996814f550bddc7545
pntech20 <pntech20@gmail.com>
2023-01-11 15:50:45 +0700

    feat: responsive page

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,21 +1,24 @@
-import { createStyles, Select, TextInput, Button, Loader } from '@mantine/core';
-import { useCallback, useState } from 'react';
-import styles from '../styles/Form.module.scss'
-import { showNotification } from '@mantine/notifications';
+import { createStyles, Select, TextInput, Button, Loader } from "@mantine/core";
+import { useCallback, useState } from "react";
+import styles from "../styles/Form.module.scss";
+import { showNotification } from "@mantine/notifications";
 
 const useStyles = createStyles((theme) => ({
   root: {
-    position: 'relative',
+    position: "relative",
   },
 
   input: {
-    height: 'auto',
+    height: "auto",
     paddingTop: 18,
+    [`@media (max-width: 700px)`]: {
+      paddingTop: 30,
+    },
   },
 
   label: {
-    position: 'absolute',
-    pointerEvents: 'none',
+    position: "absolute",
+    pointerEvents: "none",
     fontSize: theme.fontSizes.xs,
     paddingLeft: theme.spacing.sm,
     paddingTop: theme.spacing.sm / 2,
@@ -23,78 +26,92 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-export default function Form({ setImages }: { setImages: (images: string[]) => void }) {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+export default function Form({
+  setImages,
+}: {
+  setImages: (images: string[]) => void;
+}) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   async function generateImages(formData: {
-    prompt: string,
-    n: number,
-    size: string
+    prompt: string;
+    n: number;
+    size: string;
   }) {
     try {
-      setIsLoading(true)
-      const response = await fetch('/api/text2image', {
-        method: 'POST',
-        body: JSON.stringify(formData)
-      })
-      console.log(response)
+      setIsLoading(true);
+      const response = await fetch("/api/text2image", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      });
+      console.log(response);
       if (!response.ok) {
         const { errMessage } = await response.json();
         throw new Error(errMessage);
       }
       const { data } = await response.json();
-      setImages(data)
+      setImages(data);
     } catch (error: any) {
       showNotification({
         message: error?.message,
-        color: 'red',
+        color: "red",
         autoClose: 3000,
-      })
+      });
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
   }
 
   const [formData, setFormData] = useState({
     prompt: "monkeys playing football",
     n: 3,
-    size: '256x256'
-  })
+    size: "256x256",
+  });
   const setDataByKey = useCallback((key: string, value: string | null) => {
-    setFormData(curr => ({
+    setFormData((curr) => ({
       ...curr,
-      [key]: key === 'n' ? Number(value) : value
-    }))
-  }, [])
+      [key]: key === "n" ? Number(value) : value,
+    }));
+  }, []);
   // You can add these classes as classNames to any Mantine input, it will work the same
   const { classes } = useStyles();
 
   return (
     <div className={styles.form}>
-      <TextInput onChange={(e) => setDataByKey('prompt', e.target.value)} label="A text description of the desired image(s). The maximum length is 1000 characters." defaultValue={formData.prompt} classNames={classes} size="lg" />
+      <TextInput
+        onChange={(e) => setDataByKey("prompt", e.target.value)}
+        label="A text description of the desired image(s). The maximum length is 1000 characters."
+        defaultValue={formData.prompt}
+        classNames={classes}
+        size="lg"
+      />
 
       <div className={styles.flex}>
         <Select
-          style={{ marginTop: 20, zIndex: 2 }}
-          data={['256x256', '512x512', '1024x1024']}
+          style={{ marginTop: 20, zIndex: 3 }}
+          data={["256x256", "512x512", "1024x1024"]}
           defaultValue={formData.size}
           label="The size of the generated images."
           classNames={classes}
-          onChange={(val) => setDataByKey('size', val)}
+          onChange={(val) => setDataByKey("size", val)}
         />
 
         <Select
           style={{ marginTop: 20, zIndex: 2 }}
-          data={['1', '2', '3', '4', '5']}
+          data={["1", "2", "3", "4", "5"]}
           defaultValue={String(formData.n)}
           label="The number of images to generate."
           classNames={classes}
-          onChange={(val) => setDataByKey('n', val)}
+          onChange={(val) => setDataByKey("n", val)}
         />
       </div>
 
       <div className={styles.button}>
-        {isLoading ? <Loader variant='bars' /> : <Button onClick={() => generateImages(formData)}>Generate</Button>}
+        {isLoading ? (
+          <Loader variant="bars" />
+        ) : (
+          <Button onClick={() => generateImages(formData)}>Generate</Button>
+        )}
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import Form from './form'
-import Carousel from './carousel'
-import ThemeToggle from './themeToggle'
-import { useState } from 'react'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Form from "./form";
+import Carousel from "./carousel";
+import ThemeToggle from "./themeToggle";
+import { useState } from "react";
+import { Button, Menu } from "@mantine/core";
+import { IconMenu } from "@tabler/icons";
 
 export default function Home() {
-  const [images, setImages] = useState<string[]>([])
+  const [images, setImages] = useState<string[]>([]);
   return (
     <>
       <Head>
-        <title>Text2Image - Transform Your Texts into Stunning Images with OpenAI&apos;s API</title>
-        <meta name="description" content="Welcome to our website, where you can easily transform your texts into beautiful, eye-catching images using OpenAI's advanced API technology. Whether you're a business owner looking to create engaging social media posts or a blogger looking to add some visual flair to your articles, our tool makes it simple and straightforward to turn your words into art." />
+        <title>
+          Text2Image - Transform Your Texts into Stunning Images with
+          OpenAI&apos;s API
+        </title>
+        <meta
+          name="description"
+          content="Welcome to our website, where you can easily transform your texts into beautiful, eye-catching images using OpenAI's advanced API technology. Whether you're a business owner looking to create engaging social media posts or a blogger looking to add some visual flair to your articles, our tool makes it simple and straightforward to turn your words into art."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
@@ -42,10 +106,42 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}<code className={styles.code}>pntech20</code>
+              By <code className={styles.code}>pntech20</code>
             </a>
           </div>
           <ThemeToggle />
+        </div>
+        <div className={styles.mobile}>
+          <Menu shadow="md" width={200}>
+            <Menu.Target>
+              <Button>
+                <IconMenu size={14} />
+              </Button>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>
+                <p>
+                  <code className={styles.code}>Text2Image</code>
+                </p>
+              </Menu.Label>
+
+              <ThemeToggle />
+
+              <Menu.Divider />
+              <Menu.Label>
+                <div>
+                  <a
+                    href="https://github.com/pntech20"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    By <code className={styles.code}>pntech20</code>
+                  </a>
+                </div>
+              </Menu.Label>
+            </Menu.Dropdown>
+          </Menu>
         </div>
         <Form setImages={setImages} />
 
@@ -54,5 +150,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
